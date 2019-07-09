7c93edfff978b25f9f5a1bd98955085a2a76cfeb
何泽民（hezemin） <359816324@qq.com>
2019-07-09 22:25:08 +0800

    添加gitignore

--- index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const program = require('commander');
-program
-  .version('1.0.0', '-v, --version')
-  .command('init <dir>', 'generate a new project')
-  .action(function(dir, cmd){
-    console.log(dir, cmd)
-  })
-program.parse(process.argv);
